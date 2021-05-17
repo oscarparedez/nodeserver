89b3ff5b6094bed2ca99b410da0dd2bb6f38002e
oscarparedez <oaparedez17@gmail.com>
2021-05-17 17:10:48 -0600

    calcualtor and github pages replica projects added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/projects'))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html')
+app.get('/calculator', (req, res) => {
+  res.sendFile(__dirname + '/projects/calculator/index.html')
+})
+
+app.get('/githubpagesreplica', (req, res) => {
+  res.sendFile(__dirname + '/projects/githubpagesreplica/index.html')
 })
 
 app.listen(port, () => {
