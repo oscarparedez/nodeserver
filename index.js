eca9e5c2d4fc1c69a317e6bd88ddd7c8d779e7d0
oscarparedez <oaparedez17@gmail.com>
2021-05-17 17:06:08 -0600

    calcualtor dist added x2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
+app.use(express.static(__dirname + '/dist'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'dist/index.html')
+  res.sendFile(__dirname + '/dist/index.html')
 })
 
 app.listen(port, () => {
